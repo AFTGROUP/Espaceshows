86e3742aafcdb8238d3b038e33b831eafb12ffca
EmmanuelNoel <jahounoel@gmail.com>
2023-10-16 18:39:53 +0200

    Correction

--- app/Http/Controllers/API/ReservationController.php
+++ app/Http/Controllers/API/ReservationController.php
@@ -1,97 +1,81 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Models\Reservation;
-use OpenApi\Annotations as OA;
+use App\Http\Controllers\Controller;
 
-/**
- * @OA\Post(
- *     path="/reservations",
- *     tags={"Réservations"},
- *     summary="Crée une nouvelle réservation",
- *     @OA\RequestBody(
- *         required=true,
- *         @OA\JsonContent(ref="#/components/schemas/Reservation")
- *     ),
- *     @OA\Response(
- *         response=201,
- *         description="Réservation créée avec succès"
- *     ),
- *     @OA\Response(
- *         response=422,
- *         description="Erreurs de validation"
- *     ),
- * )
- */
+
 
 
 class ReservationController extends Controller
 {
-        public function index()
-        {
-            $reservations = Reservation::all();
-            return response()->json($reservations);
+
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+    public function index()
+    {
+        $reservations = Reservation::all();
+        return response()->json($reservations);
+    }
+
+    public function show($id)
+    {
+        $reservation = Reservation::find($id);
+        if (!$reservation) {
+            return response()->json(['message' => 'Réservation non trouvée'], 404);
         }
-    
-        public function show($id)
-        {
-            $reservation = Reservation::find($id);
-            if (!$reservation) {
-                return response()->json(['message' => 'Réservation non trouvée'], 404);
-            }
-            return response()->json($reservation);
+        return response()->json($reservation);
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'nom' => 'required|string|max:255',
+            'prenom' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'type_ticket' => 'required|in:Option 1,Option 2,Option 3',
+            'mode_paiement' => 'required|in:Feda Pay,KkiPay',
+        ]);
+
+        $reservation = new Reservation();
+        $reservation->fill($validatedData);
+        $reservation->save();
+
+        return response()->json(['message' => 'Réservation créée avec succès'], 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $reservation = Reservation::find($id);
+        if (!$reservation) {
+            return response()->json(['message' => 'Réservation non trouvée'], 404);
         }
-    
-        public function store(Request $request)
-        {
-            $validatedData = $request->validate([
-                'nom' => 'required|string|max:255',
-                'prenom' => 'required|string|max:255',
-                'email' => 'required|email|max:255',
-                'type_ticket' => 'required|in:Option 1,Option 2,Option 3',
-                'mode_paiement' => 'required|in:Feda Pay,KkiPay',
-            ]);
-    
-            $reservation = new Reservation();
-            $reservation->fill($validatedData);
-            $reservation->save();
-    
-            return response()->json(['message' => 'Réservation créée avec succès'], 201);
+
+        $validatedData = $request->validate([
+            'nom' => 'string|max:255',
+            'prenom' => 'string|max:255',
+            'email' => 'email|max:255',
+            'type_ticket' => 'in:Option 1,Option 2,Option 3',
+            'mode_paiement' => 'in:Feda Pay,KkiPay',
+        ]);
+
+        $reservation->update($validatedData);
+        return response()->json($reservation);
+    }
+
+    public function destroy($id)
+    {
+        $reservation = Reservation::find($id);
+        if (!$reservation) {
+            return response()->json(['message' => 'Réservation non trouvée'], 404);
         }
-    
-        public function update(Request $request, $id)
-        {
-            $reservation = Reservation::find($id);
-            if (!$reservation) {
-                return response()->json(['message' => 'Réservation non trouvée'], 404);
-            }
-    
-            $validatedData = $request->validate([
-                'nom' => 'string|max:255',
-                'prenom' => 'string|max:255',
-                'email' => 'email|max:255',
-                'type_ticket' => 'in:Option 1,Option 2,Option 3',
-                'mode_paiement' => 'in:Feda Pay,KkiPay',
-            ]);
-    
-            $reservation->update($validatedData);
-            return response()->json($reservation);
-        }
-    
-        public function destroy($id)
-        {
-            $reservation = Reservation::find($id);
-            if (!$reservation) {
-                return response()->json(['message' => 'Réservation non trouvée'], 404);
-            }
-    
-            $reservation->delete();
-            return response()->json(['message' => 'Réservation supprimée']);
-        }
+
+        $reservation->delete();
+        return response()->json(['message' => 'Réservation supprimée']);
+    }
 }
-    
-
-
-
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\RegistrationController;
 use App\Http\Controllers\API\ReservationController;
-
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\API\SubscribersController;
 use Illuminate\Http\Request;
@@ -36,6 +35,36 @@ Route::middleware(['api'])->group(function ($router) {
     Route::post('/selectRole', [RegistrationController::class, 'selectRole'])->name('selectRole');
     Route::post('register', [RegistrationController::class, 'register']); //Inscription
 
-    Route::post('/confirmAccount', [RegistrationController::class, 'confirmAccount'])->name('confirmAccount'); //Confirmation de compte par otp
-    Route::get('/generateOtp', [RegistrationController::class, 'generateOtp']);
+
+    // Route::patch('user/profile', [UserController::class, 'updateProfile']);
+});
+
+
+/**
+ * Newsletter endpoints
+ */
+Route::middleware(['api'])->group(function ($router) {
+
+    //Subscribers
+    Route::post('/subscriber', [SubscribersController::class, 'index'])->name('subscribe');
+    Route::get('/subscriber/verify/{token}/{email}', [SubscribersController::class, 'verify'])->name('subscriber_verify');
+
+    // Message to All Subscriber
+
+    Route::get('/subscriber/all', [AdminSubscriberController::class, 'show_all'])->name('admin_subscribers');
+    Route::get('/subscriber/send-email', [AdminSubscriberController::class, 'send_email'])->name('subscriber_send_email');
+    Route::post('/admin/subscriber/send-email-submit', [AdminSubscriberController::class, 'send_email_submit'])->name('subscriber_send_email_submit');
+});
+
+/**
+ * Réservation tickets endpoints
+ */
+
+Route::middleware(['api'])->group(function ($router) {
+
+    Route::get('/reservations', [ReservationController::class, 'index']); //reservation
+    Route::get('/reservations/{id}', [ReservationController::class, 'show']);
+    Route::post('/reservations', [ReservationController::class, 'store']);
+    Route::put('/reservations/{id}', [ReservationController::class, 'update']);
+    Route::delete('/reservations/{id}', [ReservationController::class, 'destroy']);
 });
