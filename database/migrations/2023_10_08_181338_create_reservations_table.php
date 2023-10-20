d83a181ab2259267374083199cc3bff5de053ce5
IDOHOU Sande Augustin <asidev7@gmail.com>
2023-10-20 11:21:03 +0100

    correction d'API reservation

--- database/migrations/2023_10_08_181338_create_reservations_table.php
+++ database/migrations/2023_10_08_181338_create_reservations_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('reservations', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->date('date_reservation');
+            $table->string('nom_utilisateur');
+            $table->string('prenom_utilisateur');
+            $table->foreign('type_ticket')->references('type_ticket')->on('tickets');
+            $table->enum('mode_paiement', ['FedaPay', 'Kkiapay']);
             $table->foreignUuid('user_id')->constrained('users');
             $table->foreignUuid('ticket_id')->constrained('tickets');
             $table->timestamps();
