90ac75d704a64c4e1925c3351adf49a011c5cb79
EmmanuelNoel <jahounoel@gmail.com>
2023-10-23 17:21:58 +0200

    User endpoints & Doc

--- database/seeders/RoleUserSeeder.php
+++ database/seeders/RoleUserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class RoleUserSeeder extends Seeder
 {
@@ -13,7 +15,19 @@ class RoleUserSeeder extends Seeder
     public function run(): void
     {
         //
-        
+
+        $users = User::take(4)->get();
+
+        // Obtenez tous les rôles de la table 'roles'
+        $roles = Role::all();
+
+        // Assurez-vous qu'il y a au moins 5 rôles disponibles
+        if (count($roles) >= 4) {
+            // Assurez-vous que le nombre de rôles disponibles correspond au nombre d'utilisateurs
+            foreach ($users as $key => $user) {
+                $user->roles()->attach($roles[$key]->id);
+            }
+        }
 
     }
 }
